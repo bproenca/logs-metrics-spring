c2e2ba6400d8888346981540341dc99bef259325
Bruno Proenca <bruno.proenca@synchro.com.br>
2021-08-05 19:54:49 -0300

    grafana

--- deploy/docker-machine/create-machine.sh
+++ deploy/docker-machine/create-machine.sh
@@ -11,6 +11,7 @@ eval "$(docker-machine env swarm-1)"
 docker swarm init --advertise-addr $(docker-machine ip swarm-1)
 
 docker-machine ssh swarm-1 "sudo mkdir -p /data/prometheus/data && sudo chown -R 1000.1000 /data"
+docker-machine ssh swarm-1 "sudo mkdir -p /data/grafana/data && sudo chown -R 1000.1000 /data"
 docker-machine ssh swarm-1 "sudo mkdir -p /data/elasticsearch/data && sudo chown -R 1000.1000 /data"
 docker-machine ssh swarm-1 "echo 'vm.max_map_count=262144' | sudo tee -a /etc/sysctl.conf && sudo sysctl -p"
 
